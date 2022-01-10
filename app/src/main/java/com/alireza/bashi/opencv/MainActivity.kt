7aaeb580c186d002ad6572136373d5ffa97dc1f7
alirezabashi98 <alirezabashi98@gmail.com>
2022-01-10 17:46:59 +0330

    Added sdk opencv to the project

--- app/src/main/java/com/alireza/bashi/opencv/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/opencv/MainActivity.kt
@@ -2,10 +2,22 @@ package com.alireza.bashi.opencv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private val tag = "mainActivity_Tag_Log"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (OpenCVLoader.initDebug()){
+            Log.i(tag,"OpenCV installed successfully")
+        }else{
+            Log.i(tag,"OpenCV is not installed")
+        }
+
     }
 }
